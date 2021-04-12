5f57ed94779334f3dc3b38c4b4c201712ac41445
unknown <joao.ganchar@gmail.com>
2021-04-11 21:42:05 -0300

    Commit

--- index.js
+++ index.js
@@ -16,70 +16,74 @@ const client = new twit({
   timeout_ms: 60 * 1000,
 });
 
-axios
-  .get(
-    `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=10&playlistId=UUOhtMAg7xh8wv_wUHMgFc-Q&key=${process.env.GOOGLE_KEY}`
-  )
-  .then((res) => {
-    const headerDate =
-      res.headers && res.headers.date ? res.headers.date : "no response date";
-    const response = res.data;
-
-    let videoID = response.items[0].contentDetails.videoId;
-
-    axios
+function logEvery2Seconds(i) {
+    setTimeout(() => {
+      axios
       .get(
-        `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${videoID}&key=${process.env.GOOGLE_KEY}`
+        `https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults=10&playlistId=UUOhtMAg7xh8wv_wUHMgFc-Q&key=${process.env.GOOGLE_KEY}`
       )
       .then((res) => {
         const headerDate =
-          res.headers && res.headers.date
-            ? res.headers.date
-            : "no response date";
+          res.headers && res.headers.date ? res.headers.date : "no response date";
         const response = res.data;
-
-        let published = response.items[0].snippet.publishedAt;
-        published = published.substring(0, published.length - 10);
-        published = published.split("-");
-
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, "0");
-        var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-        var yyyy = today.getFullYear();
-        const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-        const firstDate = new Date(published[0], published[1], published[2]);
-        const secondDate = new Date(yyyy, mm, dd);
-
-        const diffDays = Math.round(
-          Math.abs((firstDate - secondDate) / oneDay)
-        );
-
-        function BotInit() {
-          client.post(
-            "statuses/update",
-            { status: `Post Malone esta a ${diffDays} dias sem postar musica.
-
+    
+        let videoID = response.items[0].contentDetails.videoId;
+    
+        axios
+          .get(
+            `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&id=${videoID}&key=${process.env.GOOGLE_KEY}`
+          )
+          .then((res) => {
+            const headerDate =
+              res.headers && res.headers.date
+                ? res.headers.date
+                : "no response date";
+            const response = res.data;
+    
+            let published = response.items[0].snippet.publishedAt;
+            published = published.substring(0, published.length - 10);
+            published = published.split("-");
+    
+            var today = new Date();
+            var dd = String(today.getDate()).padStart(2, "0");
+            var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+            var yyyy = today.getFullYear();
+            const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+            const firstDate = new Date(published[0], published[1], published[2]);
+            const secondDate = new Date(yyyy, mm, dd);
+    
+            const diffDays = Math.round(
+              Math.abs((firstDate - secondDate) / oneDay)
+            );
+    
+            function BotInit() {
+              client.post(
+                "statuses/update",
+                { status: `Post Malone esta a ${diffDays} dias sem postar musica.
+    
 Confira a ultima musica disponivel:
-            https://www.youtube.com/watch?v=${videoID}` },
-            function (error, tweet, response) {
-              if (!error) {
-                console.log("Funcionou");
-              }else{
-                console.log(error);
-              }
+                https://www.youtube.com/watch?v=${videoID}` },
+                function (error, tweet, response) {
+                  if (!error) {
+                    console.log("Funcionou");
+                  }else{
+                    console.log(error);
+                  }
+                }
+              );
             }
-          );
-        }
-        
-        BotInit();
+            
+            BotInit();
+          })
+          .catch((err) => {
+            console.log("Erro no ultimo video: ", err.message);
+          });
       })
       .catch((err) => {
-        console.log("Erro no ultimo video: ", err.message);
+        console.log("Erro nos videos do canal: ", err.message);
       });
-  })
-  .catch((err) => {
-    console.log("Erro nos videos do canal: ", err.message);
-  });
+        logEvery2Seconds(++i);
+    }, 2000)
+}
 
-// setInterval(BotRetweet, 30 * 60 * 1000);
-
+logEvery2Seconds(0);
